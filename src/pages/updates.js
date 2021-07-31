55ce7717a296866d87ce7dc19a950475f1dfe7b2
aldousalde <85655422+aldousalde@users.noreply.github.com>
2021-07-31 23:57:03 +0800

    Debug update product functionality

--- src/pages/updates.js
+++ src/pages/updates.js
@@ -1,16 +1,24 @@
 import React, {useState, useEffect, useContext} from 'react'
-import {Card} from 'react-bootstrap'
+import {Card, Button, InputGroup, Form, Row} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import UserContext from '../userContext'
 import '../App.css'
+import Swal from 'sweetalert2'
 import{Redirect} from 'react-router-dom'
-import Update from '../components/Update'
+
+// import Update from '../components/Update'
 
 
 export default function Updates(){
 
-	const [update,setUpdate] = useState(0)
 	const {user} = useContext(UserContext)
+	const [willRedirect, setWillRedirect] = useState(false)
+	const [update,setUpdate] = useState(0)
+	const [picture, setPicture] = useState("")
+	const [name, setProductName] = useState("")
+	const [description, setDescription] = useState("")
+	const [price, setPrice] = useState(0)
+	const [isActive, setIsActive] = useState(false)
 
 	useEffect(()=>{
 		fetch(`https://cryptic-crag-81593.herokuapp.com/api/products/${localStorage.getItem('productId')}`
@@ -23,32 +31,114 @@ export default function Updates(){
 				localStorage.setItem('productPicture', product.picture)
 				localStorage.setItem('productName', product.name)
 				localStorage.setItem('productDescription', product.description)
-				localStorage.setItem('productPrice', product.price)	
-			
+				localStorage.setItem('productPrice', product.price)
+
+
+				setPicture(localStorage.productPicture)
+				setProductName(localStorage.productName)
+				setDescription(localStorage.productDescription)
+				setPrice(localStorage.productPrice)
+				setUpdate({})
+				
 			})
-			
-	},[update])
+		},[])
+
+	useEffect(()=>{
+		if(picture !== localStorage.productPicture || name!== localStorage.productName || description!== localStorage.productDescription || price !== localStorage.productPrice){
+			setIsActive(true)
+		} else {
+			setIsActive(false)
+		}
+
+		setUpdate({})
+
+	}, [picture, name, description, price])
+
+
 	
 
-	let updateContent = 
-	{
-		_id: localStorage.getItem('productId'),
-		picture: localStorage.getItem('productPicture'),
-		name: localStorage.getItem('productName'),
-		description: localStorage.getItem('productDescription'),
-		price: localStorage.getItem('productPrice')
+	function updateProduct(productId){
+
+		fetch(`https://cryptic-crag-81593.herokuapp.com/api/products/${productId}`, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+			"Authorization": `Bearer ${localStorage.getItem('token')}`
+		},
+		body: JSON.stringify({
+			picture:picture,
+			name: name,
+			description: description,
+			price: price
+		})
+
+		})
+		.then(response => response.json()) 
+		.then(data => {
+
+			console.log(data)
+			if(data.message){
+				Swal.fire({
+					icon: "error",
+					title: "Update Failed!",
+					text: data.message
+				})
+			} else {
+				
+				Swal.fire({
+					icon: "success",
+					title: "Update Successful!",
+					text: "Product Information has been updated."
+				})	
+			}
+		})
 	}
 
 	return(
-
-		user.isAdmin === false || user.isAdmin === null
-		?
-		<Redirect to="/login" />
-		:
 	
-		<>
-			<Update updateProp={updateContent} />
-		</>
+		<Card >
+			<Card.Img variant="top" className="cardImageUpdate" src={picture} />
+			<Card.Body>
+			<Form.Group>
+				<Form.Label>Picture:</Form.Label>
+				<Form.Control type="text" placeholder="Enter URL" value={picture} onChange={event=>{
+						console.log(event.target)
+						setPicture(event.target.value)}} required/>
+			</Form.Group> 
+			<Form.Group>
+				<Form.Label>Product Name:</Form.Label>
+				<Form.Control type="text" placeholder="Enter Name" value={name} onChange={event=>{
+						console.log(event.target)
+						setProductName(event.target.value)}} required/>
+			</Form.Group> 
+			<Form.Group>
+				<Form.Label>Description:</Form.Label>
+				<Form.Control type="text" placeholder="Enter Description" value={description} onChange={event=>{
+						console.log(event.target)
+						setDescription(event.target.value)}} required/>
+			</Form.Group> 
+			<Form.Group>
+				<Form.Label>Price:</Form.Label>
+				<Form.Control type="number" placeholder="Enter Price" value={price} onChange={event=>{
+						console.log(event.target)
+						setPrice(event.target.value)}} required/>
+			</Form.Group> 
+			
+			
+			</Card.Body>
+
+			{
+				isActive
+				?
+				<Button variant="success" className="mx-2" onClick={()=>updateProduct(localStorage.productId)}>Update Product Information</Button>
+				:
+				<Button variant="success" className="mx-2" disabled>Update Product Information</Button>
+
+			}
+			
+			
+				
+		</Card>
 	)
 
 }
