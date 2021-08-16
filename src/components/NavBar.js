bca35722717e45534224cb7c1a8f5b2037b172d1
aldousalde <85655422+aldousalde@users.noreply.github.com>
2021-08-16 22:35:35 +0800

    Modified NavBar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,48 +17,41 @@ export default function NavBar() {
 
   return (
     <Navbar sticky='top' bg='light' variant='light' expand='lg'>
-      <Container>
-        <Navbar.Brand href='/'>
-          <img
-            alt=''
-            src='../../public/favicon4.svg'
-            width='30'
-            height='30'
-            className='d-inline-block align-top'
-          />{' '}
-          React Bootstrap
-        </Navbar.Brand>
-      </Container>
-      <Navbar.Toggle aria-controls='basic-navbar-nav' />
-      <Navbar.Collapse id='basic-navbar-nav'>
-        <Nav className='ml-auto'>
-          <Nav.Link href='/'>Home</Nav.Link>
-          <Nav.Link href='/products'>Products</Nav.Link>
+      <Container fluid>
+        <Navbar.Brand href='/'>MyStore</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ml-auto'>
+            <Nav.Link as={NavLink} to={'/'}>
+              Home
+            </Nav.Link>
+            <Nav.Link href='/products'>Products</Nav.Link>
 
-          {user.email ? (
-            user.isAdmin ? (
-              <>
-                <Nav.Link href='/addProduct'>Add Product</Nav.Link>
-                <Nav.Link href='/orders'>Orders</Nav.Link>
-                <Nav.Link onClick={logout}>Logout</Nav.Link>
-              </>
+            {user.email ? (
+              user.isAdmin ? (
+                <>
+                  <Nav.Link href='/addProduct'>Add Product</Nav.Link>
+                  <Nav.Link href='/orders'>Orders</Nav.Link>
+                  <Nav.Link onClick={logout}>Logout</Nav.Link>
+                </>
+              ) : (
+                <>
+                  <Nav.Link href='/cart'>Cart</Nav.Link>
+                  <Nav.Link href='/orders'>Orders</Nav.Link>
+                  <Nav.Link onClick={logout} href='/login'>
+                    Logout
+                  </Nav.Link>
+                </>
+              )
             ) : (
               <>
-                <Nav.Link href='/cart'>Cart</Nav.Link>
-                <Nav.Link href='/orders'>Orders</Nav.Link>
-                <Nav.Link onClick={logout} href='/login'>
-                  Logout
-                </Nav.Link>
+                <Nav.Link href='/register'>Register</Nav.Link>
+                <Nav.Link href='/login'>Login</Nav.Link>
               </>
-            )
-          ) : (
-            <>
-              <Nav.Link href='/register'>Register</Nav.Link>
-              <Nav.Link href='/login'>Login</Nav.Link>
-            </>
-          )}
-        </Nav>
-      </Navbar.Collapse>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
