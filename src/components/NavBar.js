5aeacceba8b482ac48d71ff3268dde9f11b9638e
aldousalde <85655422+aldousalde@users.noreply.github.com>
2021-08-18 17:04:36 +0800

    Added MyAccount dropdown to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { NavLink, Link } from 'react-router-dom';
 import UserContext from '../userContext';
 
@@ -34,35 +34,95 @@ export default function NavBar() {
             {user.email ? (
               user.isAdmin ? (
                 <>
-                  <Nav.Link as={NavLink} to={'/addProduct'}>
-                    Add Product
-                  </Nav.Link>
-                  <Nav.Link as={NavLink} to={'/orders'}>
-                    Orders
-                  </Nav.Link>
-                  <Nav.Link onClick={logout}>Logout</Nav.Link>
+                  <Navbar.Collapse id='navbar-dark-example'>
+                    <Nav>
+                      <NavDropdown
+                        id='nav-dropdown-dark-example'
+                        title='MyAccount'
+                        menuVariant='dark'
+                      >
+                        <Nav.Link
+                          as={NavLink}
+                          className='nav-dropdown-link'
+                          to={'/addProduct'}
+                        >
+                          Add Product
+                        </Nav.Link>
+                        <Nav.Link
+                          as={NavLink}
+                          className='nav-dropdown-link'
+                          to={'/orders'}
+                        >
+                          Orders
+                        </Nav.Link>
+                        <Nav.Link
+                          onClick={logout}
+                          className='nav-dropdown-link'
+                        >
+                          Logout
+                        </Nav.Link>
+                      </NavDropdown>
+                    </Nav>
+                  </Navbar.Collapse>
                 </>
               ) : (
                 <>
-                  <Nav.Link as={NavLink} to={'/cart'}>
-                    Cart
-                  </Nav.Link>
-                  <Nav.Link as={NavLink} to={'/orders'}>
-                    Orders
-                  </Nav.Link>
-                  <Nav.Link onClick={logout} as={NavLink} to={'/login'}>
-                    Logout
-                  </Nav.Link>
+                  <Navbar.Collapse id='navbar-example'>
+                    <Nav>
+                      <NavDropdown id='nav-dropdown-example' title='MyAccount'>
+                        <Nav.Link
+                          as={NavLink}
+                          className='nav-dropdown-link'
+                          to={'/cart'}
+                        >
+                          Cart
+                        </Nav.Link>
+                        <Nav.Link
+                          as={NavLink}
+                          className='nav-dropdown-link'
+                          to={'/orders'}
+                        >
+                          Orders
+                        </Nav.Link>
+                        <Nav.Link
+                          onClick={logout}
+                          as={NavLink}
+                          className='nav-dropdown-link'
+                          to={'/login'}
+                        >
+                          Logout
+                        </Nav.Link>
+                      </NavDropdown>
+                    </Nav>
+                  </Navbar.Collapse>
                 </>
               )
             ) : (
               <>
-                <Nav.Link as={NavLink} to={'/register'}>
-                  Register
-                </Nav.Link>
-                <Nav.Link as={NavLink} to={'/login'}>
-                  Login
-                </Nav.Link>
+                <Navbar.Collapse id='navbar-dark-example'>
+                  <Nav>
+                    <NavDropdown
+                      id='nav-dropdown-dark-example'
+                      title='MyAccount'
+                      menuVariant='dark'
+                    >
+                      <Nav.Link
+                        as={NavLink}
+                        className='nav-dropdown-link'
+                        to={'/register'}
+                      >
+                        Register
+                      </Nav.Link>
+                      <Nav.Link
+                        as={NavLink}
+                        className='nav-dropdown-link'
+                        to={'/login'}
+                      >
+                        Login
+                      </Nav.Link>
+                    </NavDropdown>
+                  </Nav>
+                </Navbar.Collapse>
               </>
             )}
           </Nav>
